43164982f19fb32027dbe648be6e0e38976b560a
Patrick <malamashangala13@gmail.com>
2021-08-20 13:56:35 +0800

    max-sum_subarray

--- solutions/max_sum_subarray.py
+++ solutions/max_sum_subarray.py
@@ -16,7 +16,22 @@ def main():
     print("maximum of subarray of size k is: " + str(max_sum_subarray(2,[2,3,4,1,5])))
 main()
 
+# optmized solution 
+# time complexity O(N) | space complexity O(1)
+def max_sum_subarray(k, arr):
+    max_sum,window_sum = 0, 0
+    window_start = 0
 
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
 
+        if window_end >= k -1:
+            max_sum = max(max_sum, window_sum)
+            window_sum -= arr[window_start]
+            window_start += 1
+    return max_sum
 
-
+def main():
+    print("maximum of subarray of size k is: " + str(max_sum_subarray(3,[2,1,5,1,3,2,2])))
+    print("maximum of subarray of size k is: " + str(max_sum_subarray(2,[2,3,4,1,5])))
+main()
