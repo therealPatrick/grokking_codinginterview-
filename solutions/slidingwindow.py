c15dfbeccc4ad8d4fe32113f57b6337f153e130f
Patrick <malamashangala13@gmail.com>
2021-08-20 11:50:45 +0800

    sliding window pattern

---
+++ solutions/slidingwindow.py
@@ -0,0 +1,21 @@
+def find_average(k,arr):
+    result = [ ]
+    windowSum, windowStart = 0.0, 0
+    for windowEnd in range(len(arr)):
+        windowSum += arr [windowEnd] #add the next element 
+        if windowEnd >=  k - 1:
+            result.append(windowSum / k)
+            windowSum -= arr[windowStart]
+            windowStart -= arr [windowStart]
+            windowStart += 1
+    return result
+
+
+#test case
+def main():
+    result = find_average (5, [1,3,2,6,-1,4,1,8,2] )
+    print("Average  subarray of size k is : "  + str(result))
+main()
+
+
+
